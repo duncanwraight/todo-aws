016e4f4a19c4fe7e1d887cefd66ecd867be09878
Duncan Wraight <dunc@seriouslyfish.com>
2025-03-29 10:11:43 +0000

    Working struct format

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/duncanwraight/todo-aws/internal/todo"
 	"os"
 )
 
+var cmd []string
+
 func main() {
-	if len(os.Args) > 0 {
-		todo.Add(os.Args[1])
-	} else {
-		todo.List()
+	if len(os.Args) > 1 {
+		cmd = os.Args[1:]
+
+		switch cmd[0] {
+		case "add":
+			todoListItems := todo.ListItems{}
+			todoListItems.Add(os.Args[1:])
+			todoListItems.List()
+		default:
+			fmt.Println("Usage: add <item>")
+		}
 	}
+
 }
--- internal/todo/todo.go
+++ internal/todo/todo.go
@@ -1,11 +1,44 @@
 package todo
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
-func List() {
-	fmt.Println("You have a list")
+type Enum uint8
+
+const (
+	Low Enum = iota
+	Medium
+	High
+	Urgent
+)
+
+type ListItem struct {
+	Text        string
+	Priority    Enum
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time
 }
 
-func Add(item string) {
-	fmt.Printf("New item added: %s\n", item)
+type ListItems []ListItem
+
+func (listItems *ListItems) Add(items []string) {
+	listItem := ListItem{
+		Text:        strings.Join(items, " "),
+		Priority:    Low,
+		Completed:   false,
+		CreatedAt:   time.Now(),
+		CompletedAt: nil,
+	}
+	*listItems = append(*listItems, listItem)
+	fmt.Printf("New item added: %s\n", listItem.Text)
+}
+
+func (listItems *ListItems) List() {
+	for _, listItem := range *listItems {
+		fmt.Printf("%s\n", listItem.Text)
+	}
 }
